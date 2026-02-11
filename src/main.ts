ca33ca051e070891aee19587a4efaf4694be7423
Moscoso1 <parkwilfrem@gmail.com>
2026-02-11 20:07:15 +0800

    new commit

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://127.0.0.1:5500',  // Your HTML server
-    // origin: '*'  // Or allow all (less secure)
+    origin: 'http://127.0.0.1:5500',  
   });
   
   await app.listen(3000);
   console.log('Server running on http://localhost:3000');
 }
-
+//
 bootstrap();
